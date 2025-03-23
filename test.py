d928dc24c646fb943c178f54994075e8d80fe2e4
kamran umer <chary.k.tk@example.com>
2025-03-23 21:44:43 +0400

    now i have changed my email becouse contribution not appairs in github

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import os
 
 load_dotenv()  # Load variables from .env file
 
-api_key = os.getenv("put your api key here")  # Use os.getenv to avoid KeyError
+api_key = os.getenv("changed to charyktk ")  # Use os.getenv to avoid KeyError
